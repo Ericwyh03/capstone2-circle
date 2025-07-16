cdb0036f93e6c80fc392d4a37c01ed52f7b0b52c
Eric <ericwyh03@gmail.com>
2025-07-16 18:56:16 +0800

    Setup JWT auth in Laravel backend

--- backend/get_all_interests.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-require_once '../db.php';
-
-$stmt = $pdo->query("SELECT id, name FROM interests ORDER BY name");
-$interests = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($interests);
-?>
